7bab3d58d7904c12db3b882f1ef4deb197eb3689
mrx7355608 <mrx7355608@gmail.com>
2023-09-23 16:26:42 +0500

    created a data layer for users

---
+++ data/user.data.js
@@ -0,0 +1,52 @@
+import UserModel from "../models/user.model.js";
+
+// 1) Returns a list of all users in database
+async function findAll() {
+    const users = await UserModel.find(
+        {},
+        "fullname username profilePicture status"
+    ).sort("-createdAt");
+
+    return users;
+}
+
+// 2) Returns the user that has a matching username, otherwise returns null
+async function findByUsername(username) {
+    const user = await UserModel.findOne(
+        { username },
+        "fullname username profilePicture status"
+    );
+    return user;
+}
+
+// 3) Returns the user that has a matching id, otherwise returns null
+async function findById(id) {
+    const user = await UserModel.findById(
+        id,
+        "fullname username profilePicture status"
+    );
+    return user;
+}
+
+// 4) Adds a new user in database
+async function insert(user) {
+    const newUser = await UserModel.create(user);
+    return {
+        _id: newUser._id,
+        fullname: newUser.fullname,
+        status: newUser.status,
+        profilePicture: newUser.profilePicture,
+    };
+}
+
+// async function update() {}
+// async function deleteUser() {}
+
+const usersDB = {
+    findAll,
+    findByUsername,
+    findById,
+    insert,
+};
+
+export default usersDB;
